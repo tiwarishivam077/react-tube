072961b9c16441808b3549510e6da03ad4b24c81
Shivam Tiwari <shivam.tiwari@yulu.bike>
2024-08-01 13:36:56 +0530

    backup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
+import { Provider } from 'react-redux';
 import './App.css'
 import Body from './components/body';
+import store from './utils/store';
 
 
 function App() {
   return (
+    <Provider store = {store}>
     <div className=''>
     <Body/>
     </div>
+    </Provider>
   );
 }
 
--- src/components/side-bar.js
+++ src/components/side-bar.js
@@ -8,8 +8,19 @@ import ExploreVerticalNav from './explore-vertical-nav';
 import MoreFromYouTube from './more-from-youtube-vertical-nav';
 import Settings from './setting-vertical-nav';
 import AboutVerticalNav from './about-vertical-nav';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleMenu } from '../utils/appSlice';
 
 const SideBar = () => {
+  const dispatch = useDispatch()
+  const isMenuOpen = useSelector(store=>store.app.isMenuOpen)
+
+  if(!isMenuOpen) return;
+
+
+  const toggleMenuHandler=()=>{
+    dispatch(toggleMenu())
+  }
   return (
     <div className='bg-black '>
       <div className='flex flex-col w-1/3 h-ful'>
@@ -17,7 +28,10 @@ const SideBar = () => {
       <div className='flex justify-evenly  -ml-7 my-0  fixed py-4   w-[280px] h-[60px]   bg-black'>
       
          <div className='text-white'>
-          <MdOutlineMenu size={25}/>
+          <MdOutlineMenu 
+          onClick={toggleMenuHandler}
+          className='cursor-pointer'
+          size={25}/>
           </div>
 
        <div className=' h-[60px] w-[150px] -mt-4 -ml-4 flex'>
